2e29b524f971320acdc5e11f4e317d5de473b3ff
tx995976 <tx995976@Gmail.com>
2022-05-12 14:55:51 +0800

    init

---
+++ ex1_other/src/tx/game_life/Main.java
@@ -0,0 +1,12 @@
+package tx.game_life;
+
+import tx.game_life.ui.GameOfLifeFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new GameOfLifeFrame();
+
+    }
+}
